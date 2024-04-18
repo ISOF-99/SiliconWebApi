4217a25352265de9cca4a8d30b1ff4e4e78c8acc
ISOF-99 <jon_sina99@hotmail.com>
2024-04-18 16:23:06 +0200

    Add project files.

---
+++ Infrastructure/Context/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
+{
+    public DbSet<SubscribersEntity> Subscribers { get; set; }
+
+    public DbSet<CourseEntity> Courses { get; set; }
+}
---
+++ WebApi/Controllers/SubscribeController.cs
@@ -0,0 +1,50 @@
+﻿using Infrastructure.Context;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscribeController(ApiContext context) : ControllerBase
+    {
+        private readonly ApiContext _context = context;
+
+
+        [HttpPost]
+        public async Task<IActionResult> Subscribe(SubscribersEntity entity)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _context.Subscribers.AnyAsync(x => x.Email == entity.Email))
+                    return Conflict();
+
+                _context.Add(entity);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> UnSubscribe(string email)
+        {
+            if (ModelState.IsValid)
+            {
+                var subscribersEntity = await _context.Subscribers.FirstOrDefaultAsync(x => x.Email == email);
+                if (subscribersEntity == null)
+                    return NotFound();
+
+                _context.Remove(subscribersEntity);
+                await _context.SaveChangesAsync();
+                return Ok();
+
+            }
+
+            return BadRequest();
+        }
+    }
+}
